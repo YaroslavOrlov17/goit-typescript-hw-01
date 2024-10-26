b68ab884a5cbb5d9c5e81cab1e35beab5a8aa09b
Yaroslav Orlov <159253787+YaroslavOrlov17@users.noreply.github.com>
2024-10-26 09:33:23 +0300

    extends object add

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -1,3 +1,3 @@
-function merge<T extends Object, U extends Object>(objA: T, objB: U): T & U {
+function merge<T extends object, U extends object>(objA: T, objB: U): T & U {
     return Object.assign({}, objA, objB);
   }
